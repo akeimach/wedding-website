17dfc8dce9e611cc7b1e4bd4a70031364ff94711
Alyssa Keimach <alyssakeimach@gmail.com>
2019-07-25 13:46:49 -0500

    add background

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Nav/ >
+      <img src="./images/IMG_8787.jpg" style={{ maxWidth: "100%" }} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Nav: React.FC = () => {
+  return (
+    <div className="Nav">
+      <header className="Nav-header">
+        <a
+          className="Nav-link"
+          href="http://alyssakeimach.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Alyssa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Nav;
