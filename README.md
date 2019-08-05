595907a96219ec7ed658796aab615fa03f58347a
Alyssa Keimach <alyssakeimach@gmail.com>
2019-08-04 19:26:54 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# wedding-website
-Wedding website
---
+++ precache-manifest.78ac70b84a3ae69675d4b3d5b25d1a04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1171b74be5d448c915c8fecf6fb57388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05356adaff2fca7e16ed",
+    "url": "/static/css/main.24218caa.chunk.css"
+  },
+  {
+    "revision": "73e1bd1239e2fd01eeb4",
+    "url": "/static/js/2.ddf2e5f5.chunk.js"
+  },
+  {
+    "revision": "05356adaff2fca7e16ed",
+    "url": "/static/js/main.76ed0516.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
